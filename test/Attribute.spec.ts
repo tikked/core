28d565d0bab61cae792df965f1bb5c181be972c2
Nikolaj Dam Larsen <nikolaj@excolo.dk>
2019-02-21 21:58:59 +0100

    Added unit tests to Attribute

--- test/Attribute.spec.ts
+++ test/Attribute.spec.ts
@@ -1,12 +1,12 @@
-import { Attribute } from "../src/domain/Attribute";
-import { createId, createName, createDescription } from "./Fixture";
-import { expect } from "chai";
+import { Attribute } from '../src/domain/Attribute';
+import { createId, createName, createDescription } from './Fixture';
+import { expect } from 'chai';
 
 describe('Attribute', () => {
     describe('constructor', () => {
         it('should be implemented', () => {
             // Act
-            new Attribute(createId(), createName(), createDescription(), 1);
+            const attr = new Attribute(createId(), createName(), createDescription(), 1);
 
             // Assert (should not throw)
         });
@@ -18,11 +18,43 @@ describe('Attribute', () => {
             it(`should throw given invalid weight ${data}`, () => {
                 // Act
                 expect(() => {
-                    new Attribute(createId(), createName(), createDescription(), data);
+                    const attr = new Attribute(createId(), createName(), createDescription(), data);
                 })
                     // Assert
                     .to.throw(new RegExp(`weight.*${data}`));
             });
+        });
+    });
+
+    describe('Id', () => {
+        it('returns id passed in constructor', () => {
+            const id = createId();
+            const attr = new Attribute(id, createName(), createDescription(), 1);
+            expect(attr.Id).to.eql(id);
+        });
+    });
+
+    describe('Name', () => {
+        it('returns name passed in constructor', () => {
+            const name = createName();
+            const attr = new Attribute(createId(), name, createDescription(), 1);
+            expect(attr.Name).to.eql(name);
+        });
+    });
+
+    describe('Description', () => {
+        it('returns description passed in constructor', () => {
+            const description = createDescription();
+            const attr = new Attribute(createId(), createName(), description, 1);
+            expect(attr.Description).to.eql(description);
+        });
+    });
+
+    describe('Weight', () => {
+        it('returns weight passed in constructor', () => {
+            const weight = 1;
+            const attr = new Attribute(createId(), createName(), createDescription(), weight);
+            expect(attr.Weight).to.eql(weight);
         });
     });
 });
