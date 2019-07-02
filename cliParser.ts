f2907b50db0490d8cdc5e1ba7e49012f245a6a21
Alex Bondo Andersen <abondoa@gmail.com>
2019-07-02 23:09:09 +0200

    Adds help text to the cli  when using arg --help or missing arguments.

--- cliParser.ts
+++ cliParser.ts
@@ -1,14 +1,52 @@
 import * as commandLineArgs from 'command-line-args';
+import * as commandLineUsage from 'command-line-usage';
 import { Context } from './src/domain';
 
-const contextCreator = (input: string) => new Context(JSON.parse(input));
+const contextJson = (input: string) => new Context(JSON.parse(input));
+const optionDefinitions = [
+    {
+        name: 'application-environment',
+        alias: 'a',
+        type: String,
+        description: 'Name of the application environment to load. There must be a file named <application-environment>.json in the <root> folder'
+    }, {
+        name: 'context',
+        alias: 'c',
+        type: contextJson,
+        multiple: true,
+        defaultOption: true,
+        description: 'The context(s), in form of JSON object(s), which you want to test the feature flags of'
+    }, {
+        name: 'root',
+        alias: 'r',
+        type: String,
+        description: 'The root folder to search for the application environment'
+    }, {
+        name: 'help',
+        alias: 'h',
+        type: Boolean,
+        description: 'Shows this help text'
+    }
+];
 
-export function parseCli() {
-    const optionDefinitions = [
-        { name: 'application-environment', alias: 'a', type: String },
-        { name: 'context', alias: 'c', type: contextCreator, multiple: true, defaultOption: true },
-        { name: 'root', alias: 'r', type: String }
-    ];
-
+export function parseCliArgs() {
     return commandLineArgs(optionDefinitions);
+}
+
+export function showCliHelp() {
+    console.log(commandLineUsage([
+        {
+            header: 'GQ',
+            content: 'A feature flag thingy'
+        }, {
+            header: 'Synopsis',
+            content: [
+                `$ gq --root {underline folder} --application-environment {underline string} [--context] {underline contextjson} ...`,
+                '$ gq --help'
+            ]
+        }, {
+            header: 'Options',
+            optionList: optionDefinitions
+        }
+    ]))
 }
--- index.ts
+++ index.ts
@@ -1,9 +1,22 @@
 import 'reflect-metadata';
-import { parseCli } from './cliParser';
+import { parseCliArgs, showCliHelp } from './cliParser';
 import { createContainer } from './inversify.config';
 import { ApplicationEnvironmentRepository } from './src/persistency';
 
-const options = parseCli();
+const options = parseCliArgs();
+if (options.help) {
+    showCliHelp();
+    process.exit(0);
+}
+const missingArgs = ['application-environment', 'root', 'context']
+    .filter(option => !options[option]);
+
+if (missingArgs.length > 0) {
+    console.log(`Missing argument${missingArgs.length > 1 ? 's' : ''}: ${missingArgs.join(', ')}. See usage deatils below.`);
+    showCliHelp();
+    process.exit(1);
+}
+
 const repo = createContainer(options.root)
     .get<ApplicationEnvironmentRepository>(ApplicationEnvironmentRepository);
 
